fbb5815377edd4620a7478983b516d22056068aa
Francesco Neri <francesconeri95@gmail.com>
2024-11-08 14:30:01 +0100

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='NumericalCodes',
+    name='PyShockTube',
     version='1.0.1',
     author='Francesco Neri, TU Delft',
     license='MIT',
-    description='Numerical codes for simple 1D-CFD',
+    description='Resolution of Shock Tube Problems',
     install_requires=[
         'numpy',
         'matplotlib',
