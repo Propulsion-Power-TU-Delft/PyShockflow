ff0b0acb11900d0392e3525e258b7e0b4051a642
Francesco Neri <francesconeri95@gmail.com>
2024-11-11 13:56:39 +0100

    update

---
+++ TestCases/Validation_RealGas_CO2_Roe/main_high_order.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PyShockTube.shock_tube import ShockTube
+
+"""
+INPUT PARAMETERS FOR THE SHOCK-TUBE PROBLEM
+"""
+LENGTH = 10
+NX = 100
+RHOL, RHOR = 348.8, 3.488
+UL, UR = 0.0, 0.0
+PL, PR = 73760000, 737600
+TIME_MAX = 0.00275
+CFLmax = 0.25  
+FLUID = 'co2'
+FLUID_MODEL = 'real'
+NUMERICAL_SCHEME = 'roe'
+BOUNDARY_CONDITIONS = 'reflective'
+HIGH_ORDER = True
+
+
+""" 
+Solution Driver
+"""
+x = np.linspace(0, LENGTH, NX)
+dx = x[1]-x[0]
+Smax = np.sqrt(1.4*np.max([PL, PR])/np.min([RHOL, RHOR]))+np.max([UL, UR])  # brutal approximation max eigenvalue
+dtMax = CFLmax* dx / Smax
+nt = int(TIME_MAX/dtMax)
+t = np.linspace(0, TIME_MAX, nt)
+
+tube = ShockTube(x, t, FLUID, FLUID_MODEL)
+inCondDict = {'Density': np.array([RHOL, RHOR]), 'Velocity': np.array([UL, UR]), 'Pressure': np.array([PL, PR])}
+tube.InstantiateSolutionArrays()
+tube.InstantiateSolutionArraysConservatives()
+tube.InitialConditionsLeftRight(inCondDict)
+tube.SetBoundaryConditions(BOUNDARY_CONDITIONS, 0)
+
+tube.SolveSystem(flux_method=NUMERICAL_SCHEME, high_order=HIGH_ORDER)
+tube.SaveSolution(folder_name='solutions', file_name='co2_entropyfix_%i_highOrder' %NX)
+tube.ShowAnimation()
+
+
+
+
+plt.show()
--- TestCases/Validation_RealGas_CO2_Roe/plot_result.py
+++ TestCases/Validation_RealGas_CO2_Roe/plot_result.py
@@ -7,6 +7,7 @@ import os
 
 solFile = "solutions/co2_noentropyfix_500.pik"
 solFile_fix = "solutions/co2_entropyfix_500.pik"
+solFile_high = "solutions/co2_entropyfix_100_highOrder.pik"
 outFolder = 'Pictures'
 os.makedirs(outFolder, exist_ok=True)
 
@@ -19,22 +20,27 @@ with open(solFile, 'rb') as file:
     sol = pickle.load(file)
 with open(solFile_fix, 'rb') as file:
     solFix = pickle.load(file)
+with open(solFile_high, 'rb') as file:
+    solHigh = pickle.load(file)
 
 fig, ax = plt.subplots(1, 3, figsize=(17, 6))
 
 ax[0].plot(sol.xNodes, sol.solution['Density'][1:-1, -1], '-C0', label='Current') 
 ax[0].plot(solFix.xNodes, solFix.solution['Density'][1:-1, -1], '-C1', ms=1.5, label=r'Current - $s$-fix') 
 ax[0].plot(rhoRef[:,0], rhoRef[:,1], 'C2o', mfc='none', ms=5, label="D'Alessandro et al.")
+# ax[0].plot(solHigh.xNodes, solHigh.solution['Density'][1:-1, -1], '-C4', ms=1.5, label=r'Current - $s$-fix - MUSCL') 
 ax[0].set_title(r'$\rho \ \rm{[kg/m^3]}$')
 
 ax[1].plot(sol.xNodes, sol.solution['Pressure'][1:-1, -1]/1e6, '-C0')
 ax[1].plot(solFix.xNodes, solFix.solution['Pressure'][1:-1, -1]/1e6, '-C1')
 ax[1].plot(pRef[:,0], pRef[:,1], 'C2o', mfc='none', ms=5)
+# ax[1].plot(solHigh.xNodes, solHigh.solution['Pressure'][1:-1, -1]/1e6, '-C4', ms=1.5)
 ax[1].set_title(r'$P \ \rm{[MPa]}$')
 
 ax[2].plot(sol.xNodes, sol.solution['Velocity'][1:-1, -1], '-C0') 
 ax[2].plot(solFix.xNodes, solFix.solution['Velocity'][1:-1, -1], '-C1')
 ax[2].plot(uRef[:,0], uRef[:,1], 'C2o', mfc='none', ms=5) 
+# ax[2].plot(solHigh.xNodes, solHigh.solution['Velocity'][1:-1, -1], '-C4', ms=1.50)
 ax[2].set_title(r'$u \ \rm{[m/s]}$')
 
 for row in ax:
