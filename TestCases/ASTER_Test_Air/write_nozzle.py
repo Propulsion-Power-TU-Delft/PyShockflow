e2d761f3b76695e3ed54b0b488fc778938d1b680
Francesco Neri <francesconeri95@gmail.com>
2025-02-27 17:32:58 +0100

    modified nozzle shape with ASTER drawing

--- TestCases/ASTER_Test_Air/write_nozzle.py
+++ TestCases/ASTER_Test_Air/write_nozzle.py
@@ -2,30 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """
-GENERATE CSV FILE FOR A NOZZLE DESCRIBED BY A PARABOLIC SHAPE, WITH INLET AND OUTLET SECTION AREA OF SIZE 1, AND THROTTLE SECTION IN THE MIDDLE BETWEEN INLET AND OUTLET
+GENERATE CSV FILE FOR A NOZZLE RESEMBLING THE ASTER SHAPE. THE AREA VALUES ARE NORMALIZED BY THE TUBE AREA, SO THAT IN THE SIMULATION REFERENCE_AREA CAN BE LEFT TO ONE.
 """
 
-xStart = 2.95            # axial coordinate of nozzle inlet (same reference of the tube)
-xEnd = 3.05              # axial coordinate of nozzle outlet (same reference of the tube)
-A_throat = 0.5          # throat area of the nozzle (normalized by area of the tube)
-nPoints = 100            # number of points to describe the nozzle shape in the csv file
+xThroat = 3.0                           # throat axial location
+xStart = xThroat - (6+4+7.188)*1E-3     # axial coordinate of nozzle inlet 
+xEnd = xThroat + 23.412*1E-3            # axial coordinate of nozzle outlet (same reference of the tube)
+A_throat = (22/30.3)**2                 # throat area of the nozzle (normalized by area of the tube)
+A_start = 1.0                           # throat area of the nozzle inlet (normalized by area of the tube)
+A_end = 1.0                             # throat area of the nozzle outlet (normalized by area of the tube)
+nPoints = 100                           # number of points to describe the nozzle shape in the csv file
+
+
+
 
 
 x = np.linspace(xStart, xEnd, nPoints)
-Acoeff = -4*(A_throat-1)
-Bcoeff = -Acoeff
-Ccoeff = 1
-z = (x-x[0])/(x[-1]-x[0])
-Area = Acoeff*z**2 + Bcoeff*z + Ccoeff
+Area = np.interp(x, [xStart, xThroat, xEnd], [A_start, A_throat, A_end])
 
 # PLOT THE NOZZLE SHAPE
 plt.figure()
-plt.plot(x, Area)
+plt.plot([xStart, xThroat, xEnd], [A_start, A_throat, A_end], 'o', label='Control Points')
+plt.plot(x, Area, label='Quadratic fit')
 plt.xlabel('x [m]')
 plt.ylabel(r'$A/A_{tube}$ [-]')
 plt.grid(alpha=.3)
+plt.legend()
 
-with open('nozzle_%i.csv' %(A_throat*100), 'w') as file:
+with open('nozzle.csv', 'w') as file:
     file.write('x,A\n')
     for i in range(len(x)):
         file.write('%.6f,%.6f\n' %(x[i], Area[i]))
