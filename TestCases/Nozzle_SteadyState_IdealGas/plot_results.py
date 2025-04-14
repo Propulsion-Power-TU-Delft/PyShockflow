c544c837327be26bf6281fae333283774c87a272
Francesco Neri <francesconeri95@gmail.com>
2025-04-14 16:33:08 +0200

    added theory result comparison

--- TestCases/Nozzle_SteadyState_IdealGas/plot_results.py
+++ TestCases/Nozzle_SteadyState_IdealGas/plot_results.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 from PyShockTube.styles import *
+from scipy.optimize import fsolve
 
 pressureList = [45, 75, 90, 94, 97]
 pickleList = ['Results/outletPressure_%ikPa_NX_200/Results.pik' %pressure for pressure in pressureList]
@@ -17,6 +18,24 @@ for i, pickleFile in enumerate(pickleList):
     with open(pickleFile, 'rb') as file:
         solution = pickle.load(file)
     
+    if i == 0: # compute also reference
+        xArea = solution['X Coords']
+        areaRatio =  solution['Area'] / np.min(solution['Area'])
+        gammaFluid = solution['Fluid'].gmma
+        def machFunction(machLocal, areaRatioLocal, gammaFluid):
+            residual = areaRatioLocal - 1/machLocal * (2/(gammaFluid+1) * (1 + (gammaFluid-1)/2 * machLocal**2))**((gammaFluid+1)/(2*(gammaFluid-1)))
+            return residual
+
+        machTheory = np.zeros(len(xArea))
+        idThroat = np.argmin(areaRatio)
+        for iPoint in range(len(xArea)):
+            if iPoint < idThroat:
+                machTheory[iPoint] = fsolve(machFunction, 0.1, args=(areaRatio[iPoint], gammaFluid))[0]
+            else:
+                machTheory[iPoint] = fsolve(machFunction, 1.2, args=(areaRatio[iPoint], gammaFluid))[0]
+
+        ax1.plot(xArea[::10], machTheory[::10], 'ko', mfc='none' ,label=r'Supersonic reference')
+        
     xCoords = solution['X Coords'][1:-1]
     density = solution['Primitive']["Density"][1:-1,-1]
     pressure = solution['Primitive']["Pressure"][1:-1,-1]
@@ -47,6 +66,10 @@ for i, pickleFile in enumerate(pickleList):
         ax.set_xlabel(r'$x$ [-]')
         ax.legend()
         ax.grid(alpha=.3)
+
+
+
+
 
 fig1.savefig('Pictures/mach.pdf', bbox_inches='tight')
 fig2.savefig('Pictures/pressure.pdf', bbox_inches='tight')
