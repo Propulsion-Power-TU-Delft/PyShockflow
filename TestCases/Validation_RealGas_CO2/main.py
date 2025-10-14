bad37c1aa8bb6fba80c5fd44c20a32cf13f81a1e
Francesco Neri <francesconeri95@gmail.com>
2025-10-14 18:00:55 +0200

    added testcase

--- TestCases/Validation_RealGas_CO2/main.py
+++ TestCases/Validation_RealGas_CO2/main.py
@@ -1,6 +1,6 @@
 from PyShockflow.shock_tube import ShockTube
 from PyShockflow.config import Config
 
-config = Config('input_real_arabi.ini')
+config = Config('input_real_vinokur.ini')
 tube = ShockTube(config)
 tube.SolveSystem()
